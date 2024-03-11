96b742f2c0130f97629b0e0ab52c041331cf6954
konrad <konrad.dadej@gmail.com>
2024-03-11 17:20:31 +0100

    first commit

---
+++ kd_chat.py
@@ -0,0 +1,81 @@
+
+# import packages
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain_openai import ChatOpenAI
+import os
+import re
+
+def main():
+
+    # load and check openai_api_key
+    #user_api_key = os.environ.get('OPENAI_API_KEY')
+    user_api_key = input("Please provide OPENAI_API_KEY:")
+
+
+    print(f"OPENAI_API_KEY:\n{user_api_key}")
+
+    # load external csv file into row-based documents
+    csv_file_path = '/home/konrad/Documents/chatbot_x/data/alerts.csv'
+    loader = CSVLoader(file_path=csv_file_path, encoding="utf-8")
+    data = loader.load()
+
+    # embedding of the information provided in the csv file
+    print("#################################################")
+    print("Chatbot initilization...")
+    embeddings = OpenAIEmbeddings(openai_api_key=user_api_key)
+    print("    ...done")
+
+    print("Learning from provided csv...")
+    vectors = FAISS.from_documents(data, embeddings)
+    print("    ...done")
+    print("#################################################")
+    print("")
+
+    # define chain
+    chain = ConversationalRetrievalChain.from_llm(
+        llm = ChatOpenAI(temperature=0.0,
+                         model_name='gpt-3.5-turbo',
+                         openai_api_key=user_api_key),
+                         retriever=vectors.as_retriever(),
+                         return_source_documents=True
+                         )
+    
+    # send welcome message by chat
+    print(f"> Bot: Hi, I am chatbot trained by your csv file with alerts! How can I help you?")
+
+    # defince tabs for pretty printing chatbot answers
+    ls1 = "    "
+
+    # chat history
+    history = []
+    
+    # setup chat hearing in the infinite loop
+    while True:
+        user_input = input("You: ")
+        if (user_input.lower()=='exit' or user_input.lower()=='quit' or user_input.lower()=='bye'):
+            print(f"> Bot:\n{ls1}-[ANSWER]:\n{ls1}Goodbye!")
+            break
+
+        # get result from chatbot
+        result = chain({"question": user_input, "chat_history": history})
+
+        # capture alrt id's
+        alert_id = []
+        source_docs = result['source_documents']
+        for i in source_docs:
+            li = list(str(i).split("\\n"))[0]
+            li = re.sub('\D', '', li)
+            alert_id.append(li)
+
+        # print chatbot answer
+        response = result["answer"]
+        print(f"> Bot:\n{ls1}-[INFO]:\n{ls1}Text generated based on AlertId's: {alert_id}\n{ls1}-[ANSWER]:\n{ls1}{response}")
+        
+        # flush alert_id
+        alert_id = None
+
+if __name__ == "__main__":
+    main()
